1480e7a34b8a66aaac6f36fa76c5393a293e461f
Mr.YanG <yangyh@epark.com>
2019-10-28 14:33:50 +0800

    add promise.all

---
+++ es6/test.js
@@ -0,0 +1,26 @@
+let Promise = require('./index');
+let fs = require('fs');
+
+let p1 = new Promise((resolve, reject) => {
+    fs.readFile('./file/1.txt', 'utf-8', function (err, data) {
+        err ? reject(err) : resolve(data);
+    });
+})
+
+let p2 = new Promise((resolve, reject) => {
+    fs.readFile('./file/2.txt', 'utf-8', function (err, data) {
+        err ? reject(err) : resolve(data);
+    });
+})
+
+Promise.all([p1, p2]).then( (reslut) => {
+    console.log(reslut)
+}, function (error) {
+    console.log(error);
+});
+
+Promise.race([p1, p2]).then(function(result) {
+    console.log(result);
+}, function(error) {
+    console.log(error)
+});
